dca69dcd957b537af73cb53be65294a9c98df98e
anish <anishbasnet027@gmail.com>
2026-02-07 21:41:32 -0500

    Day6

---
+++ DSA/Array/Concatenation.cpp
@@ -0,0 +1,30 @@
+class Solution {
+public:
+    vector<int> getConcatenation(vector<int>& nums) {
+        int n = nums.size();
+        nums.reserve(2 * n);  // Pre-allocate to avoid reallocations
+        
+        for(int i = 0; i < n; i++) {
+            nums.push_back(nums[i]);
+        }
+        
+        return nums;
+        }
+
+    
+};
+
+// Example 1:
+
+// Input: nums = [1,2,1]
+// Output: [1,2,1,1,2,1]
+// Explanation: The array ans is formed as follows:
+// - ans = [nums[0],nums[1],nums[2],nums[0],nums[1],nums[2]]
+// - ans = [1,2,1,1,2,1]
+// Example 2:
+
+// Input: nums = [1,3,2,1]
+// Output: [1,3,2,1,1,3,2,1]
+// Explanation: The array ans is formed as follows:
+// - ans = [nums[0],nums[1],nums[2],nums[3],nums[0],nums[1],nums[2],nums[3]]
+// - ans = [1,3,2,1,1,3,2,1]
